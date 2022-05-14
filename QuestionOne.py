133a11353a372cdafb929c7679dc71863a084e9e
Millicent Malinga <71253520+MillicentMal@users.noreply.github.com>
2022-05-14 02:30:16 +0200

    Update QuestionOne.py

--- QuestionOne.py
+++ QuestionOne.py
@@ -21,7 +21,7 @@ All operations take O(1) time.
 
 from collections import OrderedDict
 
-from pyrsistent import v
+
 
 
 class LRU_Cache:
@@ -58,11 +58,10 @@ our_cache.set(3, 3);
 our_cache.set(4, 4);
 
 
-print(our_cache.get(1))      # returns 1
-print(our_cache.get(2))       # returns 2
-print(our_cache.get(9))      # returns -1 because 9 is not present in the cache
+our_cache.get(1)      # returns 1
+our_cache.get(2)    # returns 2
+our_cache.get(9)      # returns -1 because 9 is not present in the cache
 
 our_cache.set(5, 5) 
 our_cache.set(6, 6)
-print(our_cache.cache)
-print(our_cache.get(3))      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+our_cache.get(3)      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
